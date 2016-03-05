4c6fc8b68960ee2fdb421b1d7279f7055e2c1dc0
Connor Lewis <connorlewis@utexas.edu>
2016-03-05 15:10:38 -0600

    dictionary working populated

--- Starter Code/Assign4Driver.java
+++ Starter Code/Assign4Driver.java
@@ -1,6 +1,11 @@
 package assignment4;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.*;
 
 public class Assign4Driver
 {
@@ -8,7 +13,8 @@ public class Assign4Driver
     {
         // Create a word ladder solver object
         Assignment4Interface wordLadderSolver = new WordLadderSolver();
-
+        List<String> dictionary = processLinesInFile(args[0]);
+        List<String> wordLadder = processLinesInFile(args[1]);
         try 
         {
             List<String> result = wordLadderSolver.computeLadder("money", "honey");
@@ -19,4 +25,41 @@ public class Assign4Driver
             e.printStackTrace();
         }
     }
+
+/******************************************************************************
+* Method Name: processLinesInFile                                             *
+* Purpose: Opens the file specified in String filename, reads each line in it *
+*          Invokes translate () on each line in the file, and prints out the  *
+*          translated piglatin string.                                        *
+* Returns: None                                                               *
+******************************************************************************/
+public static List<String> processLinesInFile (String filename) 
+{ 
+
+	List<String> myList = new ArrayList<String>();	
+	try 
+	{
+		FileReader freader = new FileReader(filename);
+		BufferedReader reader = new BufferedReader(freader);
+		
+		for (String s = reader.readLine(); s != null; s = reader.readLine()){
+			if(!(s.charAt(0) == '*')){
+				myList.add(s.substring(0,5));
+			}
+		}
+		return myList;
+	} 
+	catch (FileNotFoundException e) 
+	{
+		System.err.println ("Error: File not found. Exiting...");
+		e.printStackTrace();
+		System.exit(-1);
+	} catch (IOException e) 
+	{
+		System.err.println ("Error: IO exception. Exiting...");
+		e.printStackTrace();
+		System.exit(-1);
+	}
+	return myList;
+}
 }
